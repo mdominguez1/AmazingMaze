33987af60b3df2d6f79b626bf030dd5f028c6fe3
Melchor Dominguez <mdmngz411@gmail.com>
2017-04-17 15:11:47 -0400

    basic stuff

---
+++ amazingMaze.js
@@ -0,0 +1,12 @@
+/**
+ * @author CJ Bland & Melchor Dominguez
+ * Building one of the most dope mazes of all time
+ * @version 4/17/2017
+ */
+/**Various different variables needed**/
+var scene, camera, renderer, canvasHeight, canvasWidth, aspRat,
+    viewLength;
+
+init();
+draw();
+renderScene();
